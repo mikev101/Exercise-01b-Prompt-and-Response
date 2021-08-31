806ab4a2fdc6061aaaf3376cbfc235873997b42e
mikev101 <vealm@iu.edu>
2021-08-31 06:17:11 -0400

    Updated main.py
    added "prompt and response" exercise content.

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+response = input("What would you like to do? ")
+
+print("You are at the " + passage_name)
+print(passage_text)
+print(response)
+
+if response == "go north":
+        print("Good job!")
